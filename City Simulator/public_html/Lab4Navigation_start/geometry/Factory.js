09c68bc3c14c2c8064244d605618cc69b9025bd0
TrifectaIII <dmaddenf@willamette.edu>
2017-11-27 11:07:03 -0800

    added doors

--- City Simulator/public_html/Lab4Navigation_start/geometry/Factory.js
+++ City Simulator/public_html/Lab4Navigation_start/geometry/Factory.js
@@ -23,4 +23,14 @@ Factory.prototype.drawFactory = function () {
     gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
     Shapes.drawPrimitive(Shapes.cylinder);
     stack.pop();
+    
+    gl.uniform1f(uColorMode, 1);
+    gl.uniform4fv(uColor, vec4(0.505, 0.360, 0.215,1));
+    stack.push();
+    stack.multiply(translate(-0.48,0,0));
+    stack.multiply(scalem(0.1,0.3,0.3));
+    stack.multiply(translate(0, 0.5, 0));
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    Shapes.drawPrimitive(Shapes.cube);
+    stack.pop();
 };
