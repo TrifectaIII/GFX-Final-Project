acdb334b9f3c52272f246d98afbc843cbc733f11
Admin <Admin@158.104.220.195>
2017-12-01 11:14:47 -0800

    Automatic Render

--- City Simulator/public_html/Lab4Navigation_start/render_scene.js
+++ City Simulator/public_html/Lab4Navigation_start/render_scene.js
@@ -102,6 +102,7 @@ function shaderSetup() {
     uProjection = gl.getUniformLocation(program, "uProjection"); // projection matrix
     uModel_view = gl.getUniformLocation(program, "uModel_view");  // model-view matrix
     uColorMode = gl.getUniformLocation(program, "uColorMode");
+    startAnimating(30);
 }
 
 var zdir = true;
@@ -112,8 +113,35 @@ var zco = 0;
 var zbar = 10;
 var xbar = 3;
 
-function render()
-{
+
+var stop = false;
+var frameCount = 0;
+var $results = $("#results");
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+//initializes the timer variables and starts the animation
+function startAnimating(fps){
+    fpsInterval = 1000/fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+//by calculating the elapsed time since the last loop
+//animate draws only if your specified fps interval has been passed!
+function animate(){
+    
+    requestAnimationFrame(animate);
+    
+    now = Date.now();
+    elapsed = now-then;
+    
+    if (elapsed>fpsInterval){
+        
+        then = now - (elapsed%fpsInterval);
+        
+        //put draw code here
+        
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     var projMat = camera.calcProjectionMat();   // Projection matrix  
@@ -232,6 +260,135 @@ function render()
     gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
     Shapes.drawPrimitive(Shapes.cube);
     stack.pop();
+        
+    }
+}
+
+
+
+
+function render()
+{
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var projMat = camera.calcProjectionMat();   // Projection matrix  
+    gl.uniformMatrix4fv(uProjection, false, flatten(projMat));
+
+    var viewMat = camera.calcViewMat();   // View matrix
+
+    stack.clear();
+    stack.multiply(viewMat);
+
+    stack.push();
+    var rotatex = rotateX(lightAngleX);
+    var rotatey = rotateY(lightAngleY);
+    var rotatexy = mult(rotatey, rotatex);
+    
+    var rotatexy = mult(rotatex, rotatey);
+
+    var rotate = mult(rotatexy, lighting.light_position);
+
+    //viewMat * (LightAngleY*lightPosition)
+    var lpos = mult(viewMat, rotate);
+    gl.uniform4fv(uLight_position, lpos);
+
+    stack.multiply(rotateY(lightAngleY));
+    stack.multiply(rotateX(lightAngleX));
+    stack.multiply(translate(lighting.light_position[0], lighting.light_position[1], lighting.light_position[2]));
+    stack.multiply(scalem(0.1, 0.1, 0.1));
+    gl.uniform1f(uColorMode, 2);
+    //console.log(uColorMode);
+    imageTexture.activate();
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    Shapes.drawPrimitive(Shapes.cube);
+    stack.pop();
+
+
+    stack.push();
+    stack.multiply(scalem(3, 0.01, 10));
+    stack.multiply(translate(0,-0.5,0));
+    gl.uniform1f(uColorMode, 2);
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    road.activate();
+    gl.uniform4fv(uColor, vec4(1.0, 1.0, 0.0, 1.0));
+    Shapes.drawPrimitive(Shapes.road);
+    stack.pop();
+    
+    stack.push();
+    stack.multiply(translate(3.5,0,0));
+    mar = new Market();
+    mar.drawMarket();
+    stack.pop();
+    
+    stack.push();
+    stack.multiply(translate(3.5,0,1));
+    off = new Office();
+    off.drawOffice();
+    stack.pop();
+    
+    stack.push();
+    stack.multiply(translate(3.5,0,2));
+    fac = new Factory();
+    fac.drawFactory();
+    stack.pop();
+    
+    
+    stack.push();
+    stack.multiply((translate(3.5, 0, 3)));
+    park = Shapes.park;
+    park.drawPark(0.05);
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    stack.pop();
+//
+    stack.push();
+    stack.multiply(translate(3.5, 0, 4));
+    apartment = Shapes.apartment;
+    apartment.drawApartment(2);
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    stack.pop();
+//
+//
+    stack.push();
+    //stack.multiply(scalem(1,1,1));
+    stack.multiply(translate(3.5, 0, 5));
+    hospital = Shapes.hospital;
+    hospital.drawHospital(2);
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    stack.pop();
+//
+//
+//    
+//    
+//    
+//    if (Math.abs(xco) > xbar){
+//        xdir = !xdir;
+//    }
+//    if (Math.abs(zco) > zbar){
+//        zdir = !zdir;
+//    }
+//    
+//    if (xdir){
+//        xco = xco + 0.1;
+//    } else {
+//        xco = xco - 0.1;
+//    }
+//    
+//
+//    if (zdir){
+//        zco = zco + 0.1;
+//    } else {
+//        zco = zco - 0.1;
+//    }
+    
+    gl.uniform1f(uColorMode, 0);
+    stack.push();
+    stack.multiply(translate(xco,0.1,zco));
+    stack.multiply(scalem(0.1,0.1,0.1));
+    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    Shapes.drawPrimitive(Shapes.cube);
+    stack.pop();
+    
+    
 
 }
 
